9287c81cb3ac860a22dcd4248a4ff56d4c4b7b73
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-11-06 22:21:16 +0900

    feat: add tests for column

---
+++ tests/ecs/test_column.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+
+#include "ecs/column.h"
+
+TEST(Column, BasicConstruct) {
+  auto int_column = Column::create_column<int>();
+  EXPECT_TRUE(int_column.is<int>());
+  EXPECT_FALSE(int_column.is<float>());
+  EXPECT_FALSE(int_column.is<std::string>());
+  EXPECT_FALSE(int_column.is<size_t>());
+}
+
+TEST(Column, AddElement) {
+  auto int_column = Column::create_column<int>();
+
+  int_column.push(15);
+  int_column.push(45);
+  int_column.push(30);
+  EXPECT_EQ(int_column.size(), 3);
+
+  EXPECT_EQ(int_column.get_data_unchecked<int>(0), 15);
+  EXPECT_EQ(int_column.get_data_unchecked<int>(1), 45);
+  EXPECT_EQ(int_column.get_data_unchecked<int>(2), 30);
+  EXPECT_EQ(int_column.get_data<int>(0)->get(), 15);  // NOLINT
+  EXPECT_EQ(int_column.get_data<int>(1)->get(), 45);  // NOLINT
+  EXPECT_EQ(int_column.get_data<int>(2)->get(), 30);  // NOLINT
+
+  // add non integer element
+  EXPECT_ANY_THROW(int_column.push(1.5));
+  EXPECT_ANY_THROW(int_column.push("hello"));
+  EXPECT_ANY_THROW(int_column.push(static_cast<size_t>(1)));
+}
+
+TEST(Column, AddTooManyElement) {
+  auto int_column = Column::create_column<int>();
+  for (int i = 0; i < 1000; ++i) {
+    int_column.push(i);
+  }
+  for (int i = 0; i < 1000; ++i) {
+    EXPECT_EQ(int_column.get_data_unchecked<int>(i), i);
+  }
+}
