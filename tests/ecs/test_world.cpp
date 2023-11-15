8b4beb9ab47514583b5307732fa8ae4d75db2224
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-11-15 20:22:49 +0900

    feat: add simple tests for world

--- tests/ecs/test_world.cpp
+++ tests/ecs/test_world.cpp
@@ -2,16 +2,9 @@
 #include "gtest/gtest.h"
 
 TEST(World, SpawnEntityWith) {
-  struct A {
-   public:
-    explicit A(std::string&& item) : item_{std::move(item)} {}
-    auto run() -> std::string { return item_; }
-
-   private:
-    std::string item_;
-  };
-
   auto world = World();
-  auto entity_location =
-      world.spawn_entity_with<A, int, std::string>(A{"Hello"}, 1, "World");
+  auto entity_location = world.spawn_entity_with<int, std::string>(1, "World");
+  EXPECT_EQ(entity_location.entity_id, 0);
+  EXPECT_EQ(entity_location.archetype_id, 0);
+  EXPECT_EQ(entity_location.table_row, 0);
 }
