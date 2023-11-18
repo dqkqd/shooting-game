07d5bd4f949b7e65a2cbd3f2f06ac292c306a2e9
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-11-18 14:43:07 +0900

    refactor: make test world's does not required concrete parameters

--- tests/ecs/test_world.cpp
+++ tests/ecs/test_world.cpp
@@ -5,9 +5,9 @@ TEST(World, SpawnEntityWith) {
   auto world = World();
   auto entity_location =
       world.spawn_entity_with<int, std::string>(1, "Hello world");
-  EXPECT_EQ(entity_location.entity_id, 0);
-  EXPECT_EQ(entity_location.archetype_id, 0);
-  EXPECT_EQ(entity_location.table_row, 0);
+
+  EXPECT_EQ((world.archetypes().get<int, std::string>()),
+            entity_location.archetype_id);
 
   auto data = world.archetypes()
                   .get_by_id_unchecked(entity_location.archetype_id)
