ef1b72b9d8d740dea4e5c89cbd901bc422c72102
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-11-19 18:53:34 +0900

    feat: add more tests for iterator

--- tests/ecs/column/test_iterator.cpp
+++ tests/ecs/column/test_iterator.cpp
@@ -1,29 +1,60 @@
 #include <gtest/gtest.h>
 
 #include "ecs/column/column.h"
+#include "ecs/column/iterator.h"
 #include "ecs/primitive.h"
 
-TEST(Column, Iterator) {
+class ColumnIteratorTest : public testing::Test {
+ protected:
   struct TestStruct {
    public:
-    explicit TestStruct(std::string &&item) : item_{item} {}
+    explicit TestStruct(std::string&& item) : item_{item} {}
     [[nodiscard]] auto item() const -> std::string { return item_; };
 
    private:
     std::string item_;
   };
 
-  auto column = Column::create_column<TestStruct>();
-  column.push<TestStruct>(TestStruct{"Hello"});
-  column.push<TestStruct>(TestStruct{" from"});
-  column.push<TestStruct>(TestStruct{" the"});
-  column.push<TestStruct>(TestStruct{" other"});
-  column.push<TestStruct>(TestStruct{" side"});
+  Column column = Column::create_column<TestStruct>();
+  ColumnIterator<TestStruct> iterator;
 
-  std::string items;
-  for (auto &iter : column.iter<TestStruct>()) {
-    items += iter.item();
+  void SetUp() override {
+    column.push<TestStruct>(TestStruct{"Hello"});
+    column.push<TestStruct>(TestStruct{" from"});
+    column.push<TestStruct>(TestStruct{" the"});
+    column.push<TestStruct>(TestStruct{" other"});
+    column.push<TestStruct>(TestStruct{" side"});
+
+    iterator = column.begin<TestStruct>();
   }
+};
 
-  EXPECT_EQ(items, "Hello from the other side");
+TEST_F(ColumnIteratorTest, Dereference) {
+  EXPECT_EQ(iterator->item(), "Hello");
+  EXPECT_EQ((*iterator).item(), "Hello");
+}
+
+TEST_F(ColumnIteratorTest, Advance) {
+  ++iterator;
+  EXPECT_EQ(iterator->item(), " from");
+}
+
+TEST_F(ColumnIteratorTest, Comparison) {
+  // iter from the same column
+  auto new_iterator = column.begin<TestStruct>();
+  EXPECT_EQ(iterator, new_iterator);
+  EXPECT_NE(++iterator, new_iterator);
+
+  // iter from different column
+  auto other = Column::create_column<TestStruct>();
+  other.push<TestStruct>(TestStruct("Hello"));
+  EXPECT_NE(other.begin<TestStruct>(), new_iterator);
+}
+
+TEST_F(ColumnIteratorTest, Loop) {
+  std::string s;
+  for (auto& it : column.iter<TestStruct>()) {
+    s += it.item();
+  }
+  EXPECT_EQ(s, "Hello from the other side");
 }
