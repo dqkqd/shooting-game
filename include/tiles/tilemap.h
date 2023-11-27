5ec79f5ebca5d7331bda1b5447d8629be7575fbe
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-11-27 22:07:49 +0900

    feat: move `TileMap` implementation to cpp file

--- include/tiles/tilemap.h
+++ include/tiles/tilemap.h
@@ -1,99 +1,36 @@
 #ifndef TILES_TILESET_H
 #define TILES_TILESET_H
 
-#include <cstddef>
-#include <utility>
-
-#include "SDL_render.h"
 #include "components/primitive.h"
-#include "components/texture.h"
 #include "ecs/world.h"
 #include "graphic.h"
 #include "tiles/parser.h"
-
-class TileSet {
- public:
-  explicit TileSet(parser::TileSetInMap data) : data_{std::move(data)} {};
-
-  void init(Graphic& graphic) {
-    auto source = CONFIG_FOLDER + data_.tile_set.image;
-    texture_ = TextureManager::add_from_file_unchecked(graphic.renderer(),
-                                                       source.c_str());
-  }
-
-  auto texture() -> SDL_Texture* { return texture_; }
-  auto data() -> parser::TileSetInMap { return data_; }
-
-  [[nodiscard]] auto position(int index) const -> Position {
-    auto w = data_.tile_set.tile_width;
-    auto h = data_.tile_set.tile_height;
-    auto x = index % data_.tile_set.columns * w;
-    auto y = index / data_.tile_set.columns * h;
-    return Position{static_cast<float>(x), static_cast<float>(y),
-                    static_cast<float>(w), static_cast<float>(h)};
-  };
-
- private:
-  parser::TileSetInMap data_;
-  SDL_Texture* texture_ = NULL;
-};
 
 struct TilePosition {
   Position src;
   Position dest;
 };
 
+class TileSet {
+ public:
+  explicit TileSet(parser::TileSetInMap data);
+
+  void init(Graphic& graphic);
+  auto texture() -> SDL_Texture*;
+  auto data() -> parser::TileSetInMap;
+  [[nodiscard]] auto position(int index) const -> Position;
+
+ private:
+  parser::TileSetInMap data_;
+  SDL_Texture* texture_ = NULL;
+};
+
 class TileMap {
  public:
-  explicit TileMap(const char* file) {
-    std::ifstream f(file);
-    json parsed = json::parse(f);
-    data_ = parsed.template get<parser::TileMap>();
+  explicit TileMap(const char* file);
 
-    for (auto tileset : data_.tile_sets) {
-      tilesets_.emplace(tileset.gid, tileset);
-    }
-  }
-
-  void init(Graphic& graphic, World& world) {
-    for (auto& [_, tileset] : tilesets_) {
-      tileset.init(graphic);
-    }
-
-    std::vector<std::tuple<SDL_Texture*, TilePosition>> textures;
-
-    for (int y = 0; y < data_.height; ++y) {
-      for (int x = 0; x < data_.width; ++x) {
-        auto pos = y * data_.width + x;
-        auto tile_id = data_.data[pos];
-        auto it = std::prev(tilesets_.upper_bound(tile_id));
-
-        auto src_position =
-            it->second.position(tile_id - it->second.data().gid);
-        auto dest_position = position(x, y);
-
-        auto texture = std::make_tuple(
-            it->second.texture(),
-            TilePosition{.src = src_position, .dest = dest_position});
-        textures.emplace_back(texture);
-      }
-    }
-
-    std::function<void()> tile_render_system = [textures, &graphic]() {
-      for (auto [query_texture, position] : textures) {
-        SDL_RenderTexture(graphic.renderer(), query_texture, &position.src.rect,
-                          &position.dest.rect);
-      }
-    };
-
-    world.add_system(std::move(tile_render_system));
-  }
-
-  [[nodiscard]] auto position(int x, int y) const -> Position {
-    auto w = static_cast<float>(data_.tile_width);
-    auto h = static_cast<float>(data_.tile_height);
-    return Position{static_cast<float>(x) * w, static_cast<float>(y) * h, w, h};
-  };
+  void init(Graphic& graphic, World& world);
+  [[nodiscard]] auto position(int x, int y) const -> Position;
 
  private:
   parser::TileMap data_;
---
+++ src/tiles/tilemap.cpp
@@ -0,0 +1,72 @@
+#include "tiles/tilemap.h"
+
+#include "components/texture.h"
+
+TileSet::TileSet(parser::TileSetInMap data) : data_{std::move(data)} {};
+
+void TileSet::init(Graphic& graphic) {
+  auto source = CONFIG_FOLDER + data_.tile_set.image;
+  texture_ = TextureManager::add_from_file_unchecked(graphic.renderer(),
+                                                     source.c_str());
+}
+
+auto TileSet::texture() -> SDL_Texture* { return texture_; }
+auto TileSet::data() -> parser::TileSetInMap { return data_; }
+
+auto TileSet::position(int index) const -> Position {
+  auto w = data_.tile_set.tile_width;
+  auto h = data_.tile_set.tile_height;
+  auto x = index % data_.tile_set.columns * w;
+  auto y = index / data_.tile_set.columns * h;
+  return Position{static_cast<float>(x), static_cast<float>(y),
+                  static_cast<float>(w), static_cast<float>(h)};
+};
+
+TileMap::TileMap(const char* file) {
+  std::ifstream f(file);
+  json parsed = json::parse(f);
+  data_ = parsed.template get<parser::TileMap>();
+
+  for (auto tileset : data_.tile_sets) {
+    tilesets_.emplace(tileset.gid, tileset);
+  }
+}
+
+void TileMap::init(Graphic& graphic, World& world) {
+  for (auto& [_, tileset] : tilesets_) {
+    tileset.init(graphic);
+  }
+
+  std::vector<std::tuple<SDL_Texture*, TilePosition>> textures;
+
+  for (int y = 0; y < data_.height; ++y) {
+    for (int x = 0; x < data_.width; ++x) {
+      auto pos = y * data_.width + x;
+      auto tile_id = data_.data[pos];
+      auto it = std::prev(tilesets_.upper_bound(tile_id));
+
+      auto src_position = it->second.position(tile_id - it->second.data().gid);
+      auto dest_position = position(x, y);
+
+      auto texture = std::make_tuple(
+          it->second.texture(),
+          TilePosition{.src = src_position, .dest = dest_position});
+      textures.emplace_back(texture);
+    }
+  }
+
+  std::function<void()> tile_render_system = [textures, &graphic]() {
+    for (auto [query_texture, position] : textures) {
+      SDL_RenderTexture(graphic.renderer(), query_texture, &position.src.rect,
+                        &position.dest.rect);
+    }
+  };
+
+  world.add_system(std::move(tile_render_system));
+}
+
+auto TileMap::position(int x, int y) const -> Position {
+  auto w = static_cast<float>(data_.tile_width);
+  auto h = static_cast<float>(data_.tile_height);
+  return Position{static_cast<float>(x) * w, static_cast<float>(y) * h, w, h};
+};
