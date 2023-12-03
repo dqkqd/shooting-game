3110b3703f034d6a7fb6fed48c7bf0b63545914b
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-12-03 17:17:47 +0900

    chore: remove unneeded [[nodiscard]]

--- src/components/position.cpp
+++ src/components/position.cpp
@@ -5,31 +5,30 @@
 
 #include "SDL_rect.h"
 
-[[nodiscard]] auto RenderPosition::top_left() const -> SDL_FPoint {
+auto RenderPosition::top_left() const -> SDL_FPoint {
   return {.x = rect.x, .y = rect.y};
 }
-[[nodiscard]] auto RenderPosition::top_right() const -> SDL_FPoint {
+auto RenderPosition::top_right() const -> SDL_FPoint {
   return {.x = rect.x + rect.w, .y = rect.y};
 }
-[[nodiscard]] auto RenderPosition::bot_left() const -> SDL_FPoint {
+auto RenderPosition::bot_left() const -> SDL_FPoint {
   return {.x = rect.x, .y = rect.y + rect.h};
 }
-[[nodiscard]] auto RenderPosition::bot_right() const -> SDL_FPoint {
+auto RenderPosition::bot_right() const -> SDL_FPoint {
   return {.x = rect.x + rect.w, .y = rect.y + rect.h};
 }
-[[nodiscard]] auto RenderPosition::points() const -> std::array<SDL_FPoint, 4> {
+auto RenderPosition::points() const -> std::array<SDL_FPoint, 4> {
   return {top_left(), top_right(), bot_left(), bot_right()};
 }
 
-[[nodiscard]] auto RenderPosition::with_x(float x) const -> RenderPosition {
+auto RenderPosition::with_x(float x) const -> RenderPosition {
   return {x, rect.y, rect.w, rect.h};
 }
-[[nodiscard]] auto RenderPosition::with_y(float y) const -> RenderPosition {
+auto RenderPosition::with_y(float y) const -> RenderPosition {
   return {rect.x, y, rect.w, rect.h};
 }
 
-[[nodiscard]] auto RenderPosition::collide(const RenderPosition& position) const
-    -> bool {
+auto RenderPosition::collide(const RenderPosition& position) const -> bool {
   auto edge_points = points();
   return std::any_of(edge_points.begin(), edge_points.end(),
                      [&position](SDL_FPoint& point) {
