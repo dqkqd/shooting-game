92f05b10d3671b44207f3baa39276a399fc68599
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-12-04 22:11:38 +0900

    feat: make camera horizontal locking without modifying y

--- src/camera.cpp
+++ src/camera.cpp
@@ -8,18 +8,13 @@ Camera::Camera(int width, int height)
 
 void Camera::center_to(const RenderPosition& position) {
   pos_.x = position.rect.x + position.rect.w / 2.0F - pos_.w / 2.0F;
-  pos_.y = position.rect.y + position.rect.h / 2.0F - pos_.h / 2.0F;
-
   pos_.x =
       std::clamp(pos_.x, 0.0F,
                  static_cast<float>(GameConfig::data().level.width) - pos_.w);
-  pos_.y =
-      std::clamp(pos_.y, 0.0F,
-                 static_cast<float>(GameConfig::data().level.height) - pos_.h);
 }
 
 auto Camera::get_position_for(const RenderPosition& position) const
     -> RenderPosition {
-  return {position.rect.x - pos_.x, position.rect.y - pos_.y, position.rect.w,
+  return {position.rect.x - pos_.x, position.rect.y, position.rect.w,
           position.rect.h};
 }
